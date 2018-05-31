6878f3f956d4bfff0eb9c369a843361fb994bbef
Pruthula.S <pruthula.s@siemens.com>
2018-05-31 20:09:39 +0530

    Empty test case

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,43 +1,21 @@
 package com.example.demo;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {WebApplicationContext.class})
+@ContextConfiguration(classes = { WebApplicationContext.class })
 @WebAppConfiguration
 public class DemoApplicationTests {
 
-	private MockMvc mockMvc;
-	
-	@Mock
-    private HelloController helloControllerMock;
-	
+
 	@Test
 	public void contextLoads() throws Exception {
-		when(helloControllerMock.index()).thenReturn("Whatever happens, happens for good");
-		 
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_PLAIN));
- 
-        verify(helloControllerMock, times(1)).index();
-        verifyNoMoreInteractions(helloControllerMock);
+		
 	}
 
 }
