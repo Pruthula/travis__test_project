2ad5f871e575bb1e8775d57746083912a59862b1
Pruthula.S <pruthula.s@siemens.com>
2018-05-31 19:57:01 +0530

    run.sh permission access for travis

--- run.sh
+++ run.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 cd $(dirname $0)
 
-cd ../complete
-
-mvn clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
 ./gradlew build
 ret=$?
 if [ $ret -ne 0 ]; then
@@ -18,13 +9,6 @@ fi
 rm -rf build
 
 cd ../initial
-
-mvn clean compile
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
 
 ./gradlew compileJava
 ret=$?
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,43 @@
 package com.example.demo;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mock;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {WebApplicationContext.class})
+@WebAppConfiguration
 public class DemoApplicationTests {
 
+	private MockMvc mockMvc;
+	
+	@Mock
+    private HelloController helloControllerMock;
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		when(helloControllerMock.index()).thenReturn("Whatever happens, happens for good");
+		 
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN));
+ 
+        verify(helloControllerMock, times(1)).index();
+        verifyNoMoreInteractions(helloControllerMock);
 	}
 
 }
