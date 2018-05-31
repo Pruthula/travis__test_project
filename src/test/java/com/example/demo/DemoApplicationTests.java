0bf799151fd266bb36c69b826a82f00a8ae6b1f9
Pruthula.S <pruthula.s@siemens.com>
2018-05-31 20:11:40 +0530

    Empty test case

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,14 +2,9 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { WebApplicationContext.class })
-@WebAppConfiguration
 public class DemoApplicationTests {
 
 
